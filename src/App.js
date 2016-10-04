780e1f1ded41266eddf9061360434c71ea6fdf99
colinmorris <colin.morris2@gmail.com>
2016-10-04 11:18:20 -0700

    stuff is happening

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import {SENTENCES} from './sentence-data';
 
+/* Map a word perplexity to a score in the range [-1, 1], 
+*/
+function ppx_scale(ppx) {
+  var floor = Math.log(1/.95);
+  var ceil = 7;
+  // XXX: Current file already has log ppx. In other places, ppx is used. Need to be consistent.
+  var log_ppx = ppx; //Math.log(ppx);
+  // normalize to range [0, 1]
+  var normalized = (ceil-log_ppx)/(ceil-floor);
+  // to [-1, 1]
+  return (normalized*2)-1;
+}
+
+class SentenceWord extends Component {
+  render() {
+    var score = ppx_scale(this.props.ppx);
+    console.log(score);
+    var barStyle = {height: Math.abs(score)*100 + "%"};
+    return (
+    <div className="SentenceWord">
+      <div className="top flank">
+        {(score >= 0) ? <div className="topBar bar" style={barStyle}></div> : null }
+      </div>
+      <div className="word">{this.props.word}</div>
+      <div className="bot flank">
+        {(score < 0) ? <div className="botBar bar" style={barStyle}></div> : null }
+      </div>
+    </div>
+    );
+  }
+}
+
 class Sentence extends Component {
   render() {
+    //var style = {height: 600, width: '90%'};
+    var style = {};
     return (
-      <div>
+      <div className="Sentence" style={style}>
       <p>Avg. perplexity={this.props.sent.ppx}</p>
-      <ul>{this.props.sent.words.map( w => {return <li>{w.word}</li>;})}</ul>
+      <div>
+        {this.props.sent.words.map( (w,i) => {
+          return <SentenceWord word={w.word} ppx={w.ppx} key={i} />;
+        })}
+      </div>
       </div>
     );
   }
@@ -29,13 +66,12 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <SentenceList sentsData={SENTENCES} />
+        <SentenceList sentsData={SENTENCES.splice(0,5)} />
       </div>
     );
   }
